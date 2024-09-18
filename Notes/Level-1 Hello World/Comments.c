e80be220039578ce6f9538fa6c8a7b57e09b8c04
AmitGouda2000 <176990007+AmitGouda2000@users.noreply.github.com>
2024-09-18 20:54:36 +0530

    Add files via upload

---
+++ Notes/Level-1 Hello World/Comments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  /* Firs-line Comment */
+  printf("Hello My first Comment\n");
+  
+  /*Multi-line Comment
+  kuch nahi bass essea hee likh raha sab kuch 
+  or mea kiya karu bolo
+  */
+  printf("Hello there!");
+  return 0;
+
+}
---
+++ Notes/Level-1 Hello World/ShowingOutput.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+  printf("My Age is : %d\n",23);
+  printf("The First Letter of Name Starts From: %c\n",'A');
+  printf("Floating-Point Of A value is : %f\n",12.5);
+  return 0;
+}
